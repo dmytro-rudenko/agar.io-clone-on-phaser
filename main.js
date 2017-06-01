4f06148ad609e9e85ef072940fff07db8e98b671
Вита и Дима <  >
2017-06-01 17:35:06 +0300

    add comments

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-var game = new Phaser.Game(width, height, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(width, height, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });//генерируем canvas на phaser, с высотой и шириной экрана пользователя.
 var player;
-var bg;
-var map_size = 4000;
-
-var bullets;
-
-var fireRate = 100;
-var nextFire = 0;
-var balls;
-
+var bg; //фон страницы
+var map_size = 4000; //размер карты
+var balls; // 
 var player_size = 1;
-
 var moveBullets;
 var colors = ["yellow", "blue", "orange", "pink"];
 
@@ -29,21 +22,20 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.time.advancedTiming = true;
-    game.time.desiredFps = 60;
+    game.time.desiredFps = 60; //fps
     game.time.slowMotion = 2.0;
 
-    bg = game.add.tileSprite(0, 0, map_size, map_size, 'background');
-    game.world.setBounds(0, 0, map_size, map_size);
-    game.stage.backgroundColor = "#000";
+    bg = game.add.tileSprite(0, 0, map_size, map_size, 'background'); //карта и картинка сетки
+    game.world.setBounds(0, 0, map_size, map_size); //размер карты
+    game.stage.backgroundColor = "#000"; //черный фон
 
     balls = game.add.group();
     balls.enableBody = true;
-
     for (var i = 0; i < 150; i++) {
         var rand = Math.floor(Math.random() * 4);
         var bounces = generateCircle(colors[rand], 10);
         var ball = balls.create(game.world.randomX, game.world.randomY, bounces);
-    }
+    } // создаем гранулы
 
     setInterval(function() {
         for (var i = 0; i < 150; i++) {
@@ -52,36 +44,28 @@ function create() {
             var bounces = generateCircle('yellow', 10);
             var ball = balls.create(game.world.randomX, game.world.randomY, bounces);
         }
-    }, 120000);
+    }, 300000); //создаем новые гранулы
 
     var bmd = generateCircle('red', 20); // рисуем круг самописной функцией диаметром 20px
 
-    player = game.add.sprite(game.world.centerX, game.world.centerY, bmd);
+    player = game.add.sprite(game.world.centerX, game.world.centerY, bmd); //создаем юнита
     game.physics.enable(player, Phaser.Physics.ARCADE);
-    player.anchor.setTo(0.5, 0.5);
-
+    player.anchor.setTo(0.5, 0.5); //центрируем
     game.camera.follow(player);
-    player.body.collideWorldBounds = true;
-
-
+    player.body.collideWorldBounds = true; //не дает юниту выйти за границы страницы
 }
-
 function update() {
-
-    player.rotation = game.physics.arcade.angleToPointer(player);
-
+    //player.rotation = game.physics.arcade.angleToPointer(player);
     game.input.addMoveCallback(function() {
         game.physics.arcade.moveToPointer(player, game.physics.arcade.distanceToPointer(player) / 2 + 250);
     });
-
     game.physics.arcade.collide(player, balls, collisionHandler, processHandler, this);
 }
-
 function render() {
     game.debug.cameraInfo(game.camera, 32, 32);
     game.debug.spriteInfo(player, 32, height - 100);
 }
-
+// функция для отрисовки круга
 function generateCircle(color, size) {
     var bitmapSize = size * 2
     var bmd = this.game.add.bitmapData(bitmapSize, bitmapSize);
@@ -92,14 +76,15 @@ function generateCircle(color, size) {
     bmd.ctx.fill();
     return bmd;
 }
-
+//Проверка столкновения
 function processHandler(player, ball) {
     return true;
 }
 
+//событие при столкновении с гранулой
 function collisionHandler(player, ball) {
-    ball.kill();
-    player_size++;
-    player.scale.set(1 + player_size / 4 / 10);
-    game.physics.arcade.moveToPointer(player, game.physics.arcade.distanceToPointer(player) / 2 + 250);
+    ball.kill(); //уничтожаем гранулу
+    player_size++; 
+    player.scale.set(1 + player_size / 4 / 10); //меняем размер юнита
+    game.physics.arcade.moveToPointer(player, game.physics.arcade.distanceToPointer(player) / 2 + 250); //вызываем фукнцию повторно, чтобы не терялась скорость если пользователь не двигает мышью
 }
